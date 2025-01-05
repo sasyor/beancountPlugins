02a23e057114bda2f0c113550e736d8bff9f30bc
sasyor <firesas@gmail.com>
2025-01-05 20:57:59 +0100

    Added balance creator

--- balance_pad_creator.py
+++ balance_pad_creator.py
@@ -3,7 +3,8 @@ import datetime
 
 __plugins__ = ["balance_pad_creator"]
 
-from beancount.parser import grammar
+from beancount.core import data
+from beancount.core.data import Balance
 
 
 class BalancePadCreator:
@@ -12,31 +13,52 @@ class BalancePadCreator:
         expr = ast.literal_eval(config_str)
         config.update(expr)
 
+        account = config["account"]
         metadata_name_balance_unit = config["metadata-name-balance-unit"]
         metadata_name_balance_time = config["metadata-name-balance-time"]
 
-        new_entries = []
+        non_relevant_entries = []
+        relevant_balances = {}
         for entry in entries:
+            if isinstance(entry, Balance) and entry.account == account:
+                self.update_relevant_balances(entry, relevant_balances)
+                continue
+
+            non_relevant_entries.append(entry)
             relevant_postings = list(
-                filter(lambda posting: posting.meta and metadata_name_balance_unit in posting.meta, entry.postings))
+                filter(lambda
+                           posting: posting.account == account and posting.meta and metadata_name_balance_unit in posting.meta,
+                       entry.postings))
+
             if len(relevant_postings) == 0:
                 continue
-            for new_entry in self.__create_entries(entry, metadata_name_balance_time, metadata_name_balance_unit,
-                                                   relevant_postings):
-                new_entries.append(new_entry)
 
-        return entries + new_entries, []
+            for new_balance in self.__create_balances(entry, metadata_name_balance_time, metadata_name_balance_unit,
+                                                      relevant_postings):
+                self.update_relevant_balances(new_balance, relevant_balances)
+
+        return non_relevant_entries + list(relevant_balances.values()), []
 
     @staticmethod
-    def __create_entries(entry, metadata_name_balance_time, metadata_name_balance_unit, relevant_postings):
-        builder = grammar.Builder()
+    def update_relevant_balances(entry, relevant_balances):
+        relevant_balance = relevant_balances.get(entry.date)
+        metadata_name_time = "balance-time"
+        if not relevant_balance:
+            relevant_balances[entry.date] = entry
+        elif relevant_balance and relevant_balance.meta[metadata_name_time] < entry.meta[metadata_name_time]:
+            relevant_balances[entry.date] = entry
+
+    @staticmethod
+    def __create_balances(entry, metadata_name_balance_time, metadata_name_balance_unit, relevant_postings):
         new_entries = []
         for posting in relevant_postings:
             date = entry.date + datetime.timedelta(days=1)
             units = posting.meta[metadata_name_balance_unit]
             meta = {metadata_name_balance_time: posting.meta[metadata_name_balance_time]}
-            balance = builder.balance(entry.meta["filename"], entry.meta["lineno"], date, posting.account, units,
-                                      None, meta)
+            balance = data.Balance(
+                data.new_metadata(entry.meta["filename"], entry.meta["lineno"], meta), date, posting.account,
+                units, None, None
+            )
             new_entries.append(balance)
         return new_entries
 
--- balance_pad_creator_test.py
+++ balance_pad_creator_test.py
@@ -1,8 +1,10 @@
+import datetime
 import unittest
 
-from beancount.core import data
-from beancount.parser import cmptest
 from beancount import loader
+from beancount.core import data
+from beancount.core.amount import A
+from beancount.parser import cmptest
 
 from balance_pad_creator import balance_pad_creator
 
@@ -19,6 +21,7 @@ class TestBalancePadCreator(cmptest.TestCase):
                 balance-time: "14:47"
         """
         config_str = ('{'
+                      '"account":"Assets:Telephone",'
                       '"metadata-name-balance-unit":"balance",'
                       '"metadata-name-balance-time":"balance-time"'
                       '}')
@@ -47,6 +50,7 @@ class TestBalancePadCreator(cmptest.TestCase):
                 balance-time: "14:47"
         """
         config_str = ('{'
+                      '"account":"Assets:Telephone",'
                       '"metadata-name-balance-unit":"balance",'
                       '"metadata-name-balance-time":"balance-time"'
                       '}')
@@ -56,6 +60,76 @@ class TestBalancePadCreator(cmptest.TestCase):
             if not isinstance(entry, data.Balance):
                 continue
             self.assertEqual(entry.meta.get("balance-time"), "14:47")
+
+    @loader.load_doc(expect_errors=True)
+    def test_balance_consolidation_no_creation(self, entries, _, options_map):
+        """
+        2013-05-31 * "Entry"
+            Assets:Bank:Checking      -100 USD
+            Assets:Telephone           100 USD
+                balance:               100 USD
+                balance-time: "14:47"
+        """
+        entries.append(data.Balance(
+            data.new_metadata(".", 1001, {"balance-time": "14:48"}), datetime.date(2013, 6, 1), "Assets:Telephone",
+            A('100 USD"'), None, None
+        ))
+        config_str = ('{'
+                      '"account":"Assets:Telephone",'
+                      '"metadata-name-balance-unit":"balance",'
+                      '"metadata-name-balance-time":"balance-time"'
+                      '}')
+        new_entries, _ = balance_pad_creator(entries, options_map, config_str)
+
+        self.assertEqualEntries(
+            """
+        2013-05-31 * "Entry"
+            Assets:Bank:Checking      -100 USD
+            Assets:Telephone           100 USD
+
+        2013-06-01 balance Assets:Telephone  100 USD
+           balance-time: "14:48"
+        """,
+            new_entries,
+        )
+        actual_entries = list(filter(lambda entry: isinstance(entry, data.Balance), new_entries))
+        self.assertEqual(len(actual_entries), 1, actual_entries)
+        self.assertEqual(actual_entries[0].meta.get("balance-time"), "14:48")
+
+    @loader.load_doc(expect_errors=True)
+    def test_balance_consolidation_with_creation(self, entries, _, options_map):
+        """
+        2013-05-31 * "Entry"
+            Assets:Bank:Checking      -100 USD
+            Assets:Telephone           100 USD
+                balance:               100 USD
+                balance-time: "14:48"
+        """
+        entries.append(data.Balance(
+            data.new_metadata(".", 1001, {"balance-time": "14:47"}), datetime.date(2013, 6, 1), "Assets:Telephone",
+            A('100 USD"'), None, None
+        ))
+        config_str = ('{'
+                      '"account":"Assets:Telephone",'
+                      '"metadata-name-balance-unit":"balance",'
+                      '"metadata-name-balance-time":"balance-time"'
+                      '}')
+        new_entries, _ = balance_pad_creator(entries, options_map, config_str)
+
+        self.assertEqualEntries(
+            """
+        2013-05-31 * "Entry"
+            Assets:Bank:Checking      -100 USD
+            Assets:Telephone           100 USD
+
+        2013-06-01 balance Assets:Telephone  100 USD
+           balance-time: "14:48"
+        """,
+            new_entries,
+        )
+        actual_entries = list(filter(lambda entry: isinstance(entry, data.Balance), new_entries))
+        self.assertEqual(len(actual_entries), 1, actual_entries)
+        self.assertEqual(actual_entries[0].meta.get("balance-time"), "14:48")
 
 
 if __name__ == '__main__':
