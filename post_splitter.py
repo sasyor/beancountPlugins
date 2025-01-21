4116d9b57b31bc0f7208a2b581400953187d74ce
sasyor <firesas@gmail.com>
2025-01-21 16:43:21 +0100

    Refactored duplicate code

--- post_splitter.py
+++ post_splitter.py
@@ -48,7 +48,7 @@ class PostSplitter:
                   and self.metadata_name_split_ratio is not None
                   and self.metadata_name_unit in post_with_split_data.meta
                   and self.metadata_name_exchange_rate in post_with_split_data.meta):
-                new_entry = self.split_cost(entry, post_with_split_data)
+                new_entry = self.split_proportional_with_cost(entry, post_with_split_data)
                 new_entries.append(new_entry)
             else:
                 new_entries.append(entry)
@@ -76,54 +76,37 @@ class PostSplitter:
                                 entry.links, new_postings)
 
     def split_proportional(self, entry, post_with_split_data):
-        max_number = 0
-        for posting in entry.postings:
-            if self.metadata_name_split_ratio in posting.meta:
-                max_number += posting.meta[self.metadata_name_split_ratio].number
+        number_to_split = -post_with_split_data.units.number
 
-        new_postings = []
-        for posting in entry.postings:
-            if posting == post_with_split_data:
-                del posting.meta[self.metadata_name_type]
-                new_postings.append(posting)
-                continue
+        return self.split_proportional_internal(entry, None, number_to_split, post_with_split_data)
 
-            number = (posting.meta[self.metadata_name_split_ratio].number / max_number
-                      * (-post_with_split_data.units.number))
-            number = self.round(number, posting.meta[self.metadata_name_split_ratio].currency)
-            new_unit = data.Amount(number,
-                                   posting.meta[self.metadata_name_split_ratio].currency)
-            new_posting = data.Posting(posting.account, new_unit, None, None, None, posting.meta)
-            new_postings.append(new_posting)
-
-        return data.Transaction(entry.meta, entry.date, entry.flag, entry.payee, entry.narration, entry.tags,
-                                entry.links, new_postings)
-
-    def split_cost(self, entry, post_with_split_data):
-        unit = post_with_split_data.meta[self.metadata_name_unit]
+    def split_proportional_with_cost(self, entry, post_with_split_data):
+        number_to_split = post_with_split_data.meta[self.metadata_name_unit].number
         exchange_rate = post_with_split_data.meta[self.metadata_name_exchange_rate]
+        new_cost = data.Cost(exchange_rate.number, exchange_rate.currency, entry.date, None)
 
+        del post_with_split_data.meta[self.metadata_name_exchange_rate]
+
+        return self.split_proportional_internal(entry, new_cost, number_to_split, post_with_split_data)
+
+    def split_proportional_internal(self, entry, new_cost, number_to_split, post_with_split_data):
         max_number = 0
         for posting in entry.postings:
             if self.metadata_name_split_ratio in posting.meta:
                 max_number += posting.meta[self.metadata_name_split_ratio].number
-
         new_postings = []
         for posting in entry.postings:
             if posting == post_with_split_data:
                 del posting.meta[self.metadata_name_type]
-                del posting.meta[self.metadata_name_exchange_rate]
                 new_postings.append(posting)
                 continue
 
-            number = posting.meta[self.metadata_name_split_ratio].number / max_number * unit.number
+            number = posting.meta[self.metadata_name_split_ratio].number / max_number * number_to_split
             number = self.round(number, posting.meta[self.metadata_name_split_ratio].currency)
             new_unit = data.Amount(number,
                                    posting.meta[self.metadata_name_split_ratio].currency)
-            new_cost = data.Cost(exchange_rate.number, exchange_rate.currency, entry.date, None)
             new_posting = data.Posting(posting.account, new_unit, new_cost, None, None, posting.meta)
             new_postings.append(new_posting)
-
         return data.Transaction(entry.meta, entry.date, entry.flag, entry.payee, entry.narration, entry.tags,
                                 entry.links, new_postings)
 
